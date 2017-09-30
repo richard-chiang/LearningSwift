d0409e87820c28835ed2c6e4f736dda4d0907d8a
Richard Chiang <shadowlairs@gmail.com>
2017-09-29 22:25:07 -0700

    Starting the project

---
+++ I-Am-Rich/I-Am-Rich/I-Am-Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I-Am-Rich
+//
+//  Created by Richard Chiang on 2017-09-29.
+//  Copyright © 2017 Richard Chiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
